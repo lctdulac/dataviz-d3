2bdabfc3ee5186012e53755c800f895650d90d82
thib-pbx <45468987+thib-pbx@users.noreply.github.com>
2020-02-21 09:16:40 +0100

    Update radial_chart.js

--- js/radial_chart.js
+++ js/radial_chart.js
@@ -91,15 +91,7 @@ var svg = d3
     var z = d3
       .scaleOrdinal()
       .domain(energyCat)
-      .range([
-        "#98abc5",
-        "#8a89a6",
-        "#7b6888",
-        "#6b486b",
-        "#a05d56",
-        "#d0743c",
-        "#ff8c00"
-      ]);
+       .range(["gold", "blue", "green", "yellow", "black", "grey", "darkgreen"]);
 
     var arc = d3
       .arc()
