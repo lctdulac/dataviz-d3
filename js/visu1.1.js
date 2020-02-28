502219b44b1724b25c58fffd908c90d0b2bce331
Mathieu Perrin <mat@mathieuperrin.com>
2020-02-28 10:22:08 +0100

    Update visu1.1.js

--- js/visu1.1.js
+++ js/visu1.1.js
@@ -200,5 +200,5 @@ function mouseover(d) {
 function mousemove(d) {
     var mouse = d3.mouse(this);
     tooltip
-        .attr("style", "left:" + (620) + "px; top:" + (750) + "px")
+        .attr("style", "left:" + (630) + "px; top:" + (750) + "px")
 }
