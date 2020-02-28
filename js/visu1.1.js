f01477aaa4d2539df7581aadc422bb02ec8cb3ed
Mathieu Perrin <mat@mathieuperrin.com>
2020-02-28 10:14:58 +0100

    Update visu1.1.js

--- js/visu1.1.js
+++ js/visu1.1.js
@@ -131,8 +131,8 @@ d3.csv(data, function(error, raw) {
     // text label for the y axis
   	svg.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2)-20)
+      .attr("y", 0 - margin.left-10)
+      .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
       .text("Energy (MWh)"); 
